4cfd42e09f7f0c0a7a105f36ce8d812f3a393057
benjaminpettus <benjaminpettus@gmail.com>
2017-03-27 15:39:46 -0700

    make change completed

--- src/makeChange.js
+++ src/makeChange.js
@@ -1,3 +1,30 @@
 export default function makeChange({price, amountGiven}) {
-  // your code here
+
+  let change = {
+    quarters: 0,
+    dimes: 0,
+    nickels: 0,
+    pennies: 0,
+  }
+
+  let remainder;
+
+  if( price > amountGiven ){
+      console.log("not enough money")
+  }
+
+  const difference = amountGiven - price
+
+  change.quarters += parseInt( difference /25 )
+  remainder = difference - change.quarters * 25
+
+  change.dimes += parseInt( remainder / 10 )
+  remainder = remainder - change.dimes * 10
+
+  change.nickels += parseInt( remainder / 5 );
+  remainder = remainder - change.nickels * 5
+
+  change.pennies += remainder
+
+  return change
 }
