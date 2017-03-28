11eada25e00bea0a67a9a1da6e9ad4d297e4b431
benjaminpettus <benjaminpettus@gmail.com>
2017-03-28 09:50:43 -0700

    switching to fibonacci branch

---
+++ test/fibonacci_test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+import fibonacci from '../src/fibonacci'
+
+describe('fibonacci()', function(){
+
+  it('should be a function', function(){
+    expect(fibonacci).to.be.a('function')
+  })
+})
