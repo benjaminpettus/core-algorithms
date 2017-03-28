b8fad4ea4df0078e00c22db5d54d7b3f958ebf05
benjaminpettus <benjaminpettus@gmail.com>
2017-03-28 15:12:27 -0700

    starting setSymmetricDifference

---
+++ test/setSymmetricDifference_test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+import setSymmetricDifference from '../src/setSymmetricDifference.js'
+
+describe('setSymmetricDifference()', function(){
+
+  it('should be a function', function() {
+    expect(setSymmetricDifference).to.be.a('function')
+  })
+})
