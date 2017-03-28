b42cadc8e094a7c30f5eafb4a9a537e980677328
benjaminpettus <benjaminpettus@gmail.com>
2017-03-28 15:32:45 -0700

    completed setSymmetricDifference

--- src/setSymmetricDifference.js
+++ src/setSymmetricDifference.js
@@ -1,3 +1,20 @@
-export default function setSymmetricDifference() {
-  
+export default function setSymmetricDifference(set1, set2) {
+  if(!Array.isArray(set1) || !Array.isArray(set2)){
+  return 'sets must be arrays'
+}
+
+let newSet = []
+for (let number in set1){
+  if(!set2.includes(set1[number])){
+    newSet.push(set1[number])
+  }
+}
+
+for (let number in set2 ) {
+
+  if(!set1.includes(set2[number])){
+     newSet.push(set2[number])
+  }
+}
+return newSet
 }
