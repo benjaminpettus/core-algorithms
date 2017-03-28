6f23754610193c00587d654c3a3c78cafee0830d
benjaminpettus <benjaminpettus@gmail.com>
2017-03-28 11:52:57 -0700

    setUnion complete

---
+++ test/setUnion_test.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai'
+import setUnion from '../src/setUnion'
+
+describe('setUnion()', function() {
+
+  it('should be a function', function() {
+    expect(setUnion).to.be.a('function')
+  })
+
+  it('should return the union of two sets', function() {
+    const a = [1, 2, 3, 4]
+    const b = [2, 4, 6, 8]
+    expect(setUnion(a,b)).to.eql([1, 2, 3, 4, 6, 8])
+  })
+})
