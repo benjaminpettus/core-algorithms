e49c5eda09a254ca67260e556a8f77e87df949c2
benjaminpettus <benjaminpettus@gmail.com>
2017-03-30 15:02:13 -0700

    bubbleSort complete

--- src/bubbleSort.js
+++ src/bubbleSort.js
@@ -1,3 +1,15 @@
-export default function bubbleSort(){
-  
+export default function bubbleSort(arr){
+  if(!Array.isArray(arr)){
+    return 'input must be an array'
+  }
+  for (let i = 0; i < arr.length; i++){
+     if (arr[i] > arr[i + 1]){
+       let holder = arr[i]
+       arr[i] = arr[i + 1]
+       arr[i + 1] = holder
+       bubbleSort(arr)
+     }
+   }
+   return arr
+
 }
